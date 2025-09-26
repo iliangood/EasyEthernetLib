47bf54ffea689fd25137c8fbda54cfa210be1282
iliangood <ilia191168@gmail.com>
2025-09-26 19:57:52 +0300

    Перенес класс в библиотеку

---
+++ src/easyEthernetLib.h
@@ -0,0 +1,44 @@
+#if not defined EASY_ETHERNET_LIB
+#define EASY_ETHERNET_LIB
+
+#include "utils.h"
+
+class DataTransmitter {
+private:
+  uint8_t mac[6];
+  IPAddress targetIP;
+  bool lockTargetIP;
+  EthernetUDP Udp;
+  const char* magicString;
+  size_t magicStringLength;
+  unsigned int port;
+#if not USE_DHCP
+  IPAddress ip;
+#endif
+public:
+#if USE_DHCP
+  DataTransmitter(const byte* mac, unsigned int port, const char* magicString);
+#else
+  DataTransmitter(const byte* mac, unsigned int port, IPAddress ip, const char* magicString);
+#endif
+  void setLockTargetIP(bool lock);
+  void setTargetIP(IPAddress targetIP, bool lockTargetIP = true);
+
+  bool isValid();
+
+  int init();
+
+  int sendData(const byte* data, int dataSize);
+
+  int sendData(const char* data);
+
+  size_t receiveData(byte* buffer, int maxSize);
+
+  IPAddress getTargetIP();
+
+#if not USE_DHCP
+  void maintain(); //Обновление DHCP
+#endif
+};
+
+#endif
---
+++ src/utils.h
@@ -0,0 +1,46 @@
+#if not defined UTILS_H
+#define UTILS_H
+
+#define DEBUG_LEVEL_NOTHING 0
+#define DEBUG_LEVEL_ERRORS 1
+#define DEBUG_LEVEL_WARNINGS 2
+#define DEBUG_LEVEL_INFO 3
+#define DEBUG_LEVEL_VERBOSE 4
+
+#if not defined DEBUG_LEVEL
+#define DEBUG_LEVEL DEBUG_LEVEL_ERRORS
+#endif
+
+#define DEBUG_LEVEL DEBUG_LEVEL_VERBOSE
+
+#define LOG_ERROR(...) do { \
+    if (DEBUG_LEVEL >= DEBUG_LEVEL_ERRORS) { \
+        Serial.print("[ERROR] "); \
+        Serial.println(__VA_ARGS__); \
+    } \
+} while (0)
+
+#define LOG_WARNING(...) do { \
+    if (DEBUG_LEVEL >= DEBUG_LEVEL_WARNINGS) { \
+        Serial.print("[WARNING] "); \
+        Serial.println(__VA_ARGS__); \
+    } \
+} while (0)
+
+#define LOG_INFO(...) do { \
+    if (DEBUG_LEVEL >= DEBUG_LEVEL_INFO) { \
+        Serial.print("[INFO] "); \
+        Serial.println(__VA_ARGS__); \
+    } \
+} while (0)
+
+#define LOG_VERBOSE(...) do { \
+    if (DEBUG_LEVEL >= DEBUG_LEVEL_VERBOSE) { \
+        Serial.print("[VERBOSE] "); \
+        Serial.println(__VA_ARGS__); \
+    } \
+} while (0)
+
+#define clamp(num, minV, maxV) (max(min((num), (maxV)), (minV)))
+
+#endif
